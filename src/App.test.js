9cb292b00cf098e498079d6c854704f3c1ae9852
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-02-16 22:25:35 +0000

    Wrote test for input element

--- src/App.test.js
+++ src/App.test.js
@@ -1,2 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+
+test('Inputs should initially be empty', () => {
+  render(<App />);
+  const emailInputElement = screen.getByRole('textbox');
+  expect(emailInputElement.value).toBe('');
+});
