fd548ca39e4c51a86f915047b934dddfaf98a35c
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-02-18 23:51:01 +0000

    Created form input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <div className='container my-5'>
+      <form>
+        <div className='mb-3'>
+          <label htmlFor='email' className='form-label'>
+            Email Address
+          </label>
+          <input
+            type='email'
+            id='email'
+            name='email'
+            className='form-control'
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default App;
