f5f67834806949f49a1724461f285102f52e55fb
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-02-19 18:33:38 +0000

    Added password input test

--- src/App.js
+++ src/App.js
@@ -15,6 +15,29 @@ function App() {
             className='form-control'
           />
         </div>
+
+        <div className='mb-3'>
+          <label htmlFor='password' className='form-label'>
+            Password
+          </label>
+          <input
+            type='password'
+            id='password'
+            name='password'
+            className='form-control'
+          />
+        </div>
+        <div className='mb-3'>
+          <label htmlFor='confirm-password' className='form-label'>
+            Confirm Password
+          </label>
+          <input
+            type='password'
+            id='confirm-password'
+            name='confirm-password'
+            className='form-control'
+          />
+        </div>
       </form>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,10 @@ import App from './App';
 test('Inputs should initially be empty', () => {
   render(<App />);
   const emailInputElement = screen.getByRole('textbox');
+  const passwordInputElement = screen.getByLabelText('Password');
+  const confirmPasswordInputElement =
+    screen.getByLabelText(/confirm password/i);
   expect(emailInputElement.value).toBe('');
+  expect(passwordInputElement.value).toBe('');
+  expect(confirmPasswordInputElement.value).toBe('');
 });
