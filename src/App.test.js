d28a026faab6a5ec7e2faaf0b8e4c102411fd882
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-02-20 22:47:04 +0000

    Added user event to check for email

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 test('Inputs should initially be empty', () => {
@@ -10,4 +11,13 @@ test('Inputs should initially be empty', () => {
   expect(emailInputElement.value).toBe('');
   expect(passwordInputElement.value).toBe('');
   expect(confirmPasswordInputElement.value).toBe('');
+});
+
+test('should be able to type an email', () => {
+  render(<App />);
+  const emailInputElement = screen.getByRole('textbox', {
+    name: /email/i,
+  });
+  userEvent.type(emailInputElement, 'lawrence@gmail.com');
+  expect(emailInputElement.value).toBe('lawrence@gmail.com');
 });
